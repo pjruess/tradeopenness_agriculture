9e10c756e36ebd8e402767a664fa9eefbd9cf17a
Akshay163 <apandit2@illinois.edu>
2019-02-14 14:40:41 -0600

    added collect_data.r file

--- collect_data.r
+++ collect_data.r
@@ -3,7 +3,7 @@
 # Spring 2019
 
 # Load necessary libraries
-
+library(XLConnect)
 
 # Load in functions from copies of Qian's scripts
 
@@ -11,18 +11,20 @@
 ### Read in all data
 
 # Read in Capital Stocks at Current PPPs (2011)
-ck <- read.csv('rawdata/pwt90.xlsx')
-ck <- # select correct sheet
+ck <- readxl::read_xlsx('rawdata/pwt90.xlsx')
+#ck <- # select correct sheet
 ck <- ck[,c('countrycode','country','year','ck')]
 print(head(ck))
-ck.to_csv('cleandata/pwt90.csv') #save cleaned version
+write.csv(ck,file='cleandata/pwt90.csv') #save cleaned version
 
 
 # Read in Population data...
-
-
+pop<- readxl::read_xlsx('rawdata/WPP2017_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES_1.xlsx', sheet = 1)
+col_drop<- c('Index', 'Variant', 'Notes')
+pop<-pop[,!names(pop) %in% col_drop]
+write.csv(pop,file='cleandata/population_data.csv') #save cleaned version
 ### Merge datasets on country and year
 df <- 
 
 # Save merged df as new .csv file
-df.to_csv('results/input_data_clean.csv')
+write.csv('results/input_data_clean.csv')
