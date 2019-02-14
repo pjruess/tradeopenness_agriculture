0b0eb02f09755a5ffdbfb07ec4dd13afe3587637
paul <pjruess@gmail.com>
2019-02-14 15:18:06 -0600

    saving files for different data subsets

--- merge_qian_data.r
+++ merge_qian_data.r
@@ -5,3 +5,15 @@ df2 <- read.csv('cleandata/main_df1_0621.csv')
 df <- merge(df1,df2)
 
 write.csv(df,'cleandata/qian_alldata.csv',row.names=FALSE)
+
+df_s0 <- df[,c('Year','iso_o')]
+write.csv(df_so,'cleandata/qian_stage0_data.csv',row.names=FALSE)
+
+df_s1 <- df[,c('Year','iso_o')]
+write.csv(df_so,'cleandata/qian_stage0_data.csv',row.names=FALSE)
+
+df_s0 <- df[,c('Year','iso_o')]
+write.csv(df_so,'cleandata/qian_stage0_data.csv',row.names=FALSE)
+
+df_s0 <- df[,c('Year','iso_o')]
+write.csv(df_so,'cleandata/qian_stage0_data.csv',row.names=FALSE)
