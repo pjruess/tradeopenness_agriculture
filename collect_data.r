4d5744662ca0db645f136b9884a6750f2d3c639f
Akshay163 <apandit2@illinois.edu>
2019-02-14 14:52:20 -0600

    collect data file updated

--- collect_data.r
+++ collect_data.r
@@ -9,16 +9,28 @@ library(XLConnect)
 
 
 ### Read in all data
-
+#Read in Distance between countries
+dist<-readxl::read_xls('rawdata/dist_cepii.xls')
+dist<- dist[,c('iso_o','iso_d','contig','dist', 'distcap')]
+dist<-na.omit(dist)
+print(head(dist))
+write.csv(dist,file='cleandata/dist_cepii.csv') #save cleaned version
+#Read in Geometric Variables(Latitude, Longitude, area, dummy variables etc)
+geo<-readxl::read_xls('rawdata/geo_cepii.xls')
+geo<- geo[,c('country','iso3','area','dis_int', 'lat', 'lon','landlocked')]
+geo<-na.omit(geo)
+print(head(geo))
+write.csv(geo,file='cleandata/geo_cepii.csv') #save cleaned version
 # Read in Capital Stocks at Current PPPs (2011)
-ck <- readxl::read_xlsx('rawdata/pwt90.xlsx')
+ck <- readxl::read_xlsx('rawdata/pwt90.xlsx',sheet=3)
 #ck <- # select correct sheet
-ck <- ck[,c('countrycode','country','year','ck')]
+ck <- ck[,c('countrycode','country','ck')]
+ck<-na.omit(ck)
 print(head(ck))
 write.csv(ck,file='cleandata/pwt90.csv') #save cleaned version
 
 
-# Read in Population data...
+# Read in Population data(in thousands)...
 pop<- readxl::read_xlsx('rawdata/WPP2017_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES_1.xlsx', sheet = 1)
 col_drop<- c('Index', 'Variant', 'Notes')
 pop<-pop[,!names(pop) %in% col_drop]
