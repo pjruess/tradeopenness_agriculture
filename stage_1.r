11ca08238f3301659788c25050649986814f11d3
Akshay163 <apandit2@illinois.edu>
2019-05-20 03:28:44 -0500

    stage0 updated and stage 1 added

--- stage_1.r
+++ stage_1.r
@@ -14,22 +14,9 @@ library(Matrix)
 cat(rankMatrix(ver1_step1), "\n")
 # Load in functions from copies of Qian's scripts
 #EStimates constructed trade openness
-predopen <- function(fo, y_bigeo){
-  # y_bigeo = df1_curr
-  hat_open <- as.data.frame(predict(fo, newdata = y_bigeo))
-  pred <- cbind(y_bigeo[,1:3], hat_open)
-  names(pred)[4] <- "log_hat_open"
-  pred$hat_open <- exp(pred$log_hat_open)
-  hat_totalopen <- sqldf("SELECT iso_o,Year, sum(hat_open)
-                         FROM pred
-                         GROUP BY iso_o, Year")
-  names(hat_totalopen) <- c("ISO", "Year","openness_hat")
-  #write.csv(hat_totalopen,"hat_totalopen.csv", row.names = F)
-  return(hat_totalopen)
-}
 
 # Read in data
 df <- read.csv('cleandata/input_data_clean_stage1.csv') # all input data (from collect_data.r script)
-pa_formula1_1 = "log(TO) ~ openness_hat+ log(AperP) + log(ckperP) + log(pop_o) + rainfall + temperature + factor(iso_o) + factor(year)"
+pa_formula1_1 = "TO ~ openness_hat+ log(AperP) + log(ckperP) + log(pop_o) + rainfall + temperature + factor(iso_o) + factor(year)"
 ver1_step1<- lm(formula = pa_formula1_1, data = df)
 hat_open <- as.data.frame(predict(ver1_step1, newdata = df))
