3aed6e0bd5f41e37477160f0968ea38cbbf06b43
Akshay163 <apandit2@illinois.edu>
2019-05-20 03:39:14 -0500

    stage0 updated and stage 1 added

--- stage_1.r
+++ stage_1.r
@@ -16,7 +16,10 @@ cat(rankMatrix(ver1_step1), "\n")
 #EStimates constructed trade openness
 
 # Read in data
-df <- read.csv('cleandata/input_data_clean_stage1.csv') # all input data (from collect_data.r script)
+df <- read.csv('results/input_data_clean_stage1.csv') # all input data (from collect_data.r script)
 pa_formula1_1 = "TO ~ openness_hat+ log(AperP) + log(ckperP) + log(pop_o) + rainfall + temperature + factor(iso_o) + factor(year)"
 ver1_step1<- lm(formula = pa_formula1_1, data = df)
-hat_open <- as.data.frame(predict(ver1_step1, newdata = df))
+hat_realopen <- as.data.frame(predict(ver1_step1, newdata = df))
+colnames(hat_realopen)<-'hat_realopen'
+df$hat_realopen<-hat_realopen$hat_realopen
+write.csv(df,file='results/input_data_clean_stage2.csv',row.names = FALSE)
