4af4689258da31b604afa8bb8f3d3281d6f3fcac
Akshay163 <apandit2@illinois.edu>
2019-05-20 15:08:49 -0500

    stage 2 updated

--- stage_2.r
+++ stage_2.r
@@ -5,7 +5,14 @@
 # Spring 2019
 
 # Load necessary libraries
-
+library(sqldf)
+library(AER)
+library(MASS)
+library(gtools)
+library(stringr)
+library(stargazer)
+library(xtable)
+library(reshape2)
 
 # Load in functions from copies of Qian's scripts
 
@@ -16,10 +23,10 @@ nutrients<-read.csv('cleandata/main_df2_wq_all.csv')
 nutrients<-nutrients[-c(3:5)]
 df<-merge(df,nutrients,by.x=c('iso_o','year'),by.y=c('ISO','Year'))
 df<-na.omit(df)
-
-pa_formula1_2 = "log(N) ~ hat_realopen + log(AperP) + log(ckperP) + log(pop_o) + rainfall + temperature + factor(iso_o) + factor(year)"
-ver1_step2<- lm(formula = pa_formula1_2, data = df)
 df<-df[which(df[c(13)]!=0),]
+pa_formula1_2 = "log(N) ~ hat_realopen + log(AperP) + log(ckperP) + log(pop_o) + rainfall + temperature + factor(iso_o) + factor(year) |. -TO + hat_realopen"
+ver1_step2<- ivreg(formula = pa_formula1_2, data = df)
+
 # Regress agricultural variables on estimated real trade openness + control variables, country and time fixed effects, and error (same as stage 1)
 
 
